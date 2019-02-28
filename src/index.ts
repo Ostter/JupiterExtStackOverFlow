c3a985c6b043986c3e73711dd3269028f0312065
Konstantin <k.sharov@skoltech.ru>
2019-02-28 12:25:13 +0300

    add styles

--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,70 @@
-import {
-  JupyterLab, JupyterLabPlugin
-} from '@jupyterlab/application';
+import { JupyterLab, JupyterLabPlugin } from "@jupyterlab/application";
+import { ICommandPalette } from "@jupyterlab/apputils";
+import { Widget } from "@phosphor/widgets";
 
-import {
-    ICommandPalette
-} from '@jupyterlab/apputils';
-
-import '../style/index.css';
-
-import {
-    Widget
-} from '@phosphor/widgets';
+import "../style/index.css";
 
 /**
  * Initialization data for the ext01 extension.
  */
 
 const extension: JupyterLabPlugin<void> = {
-    id: 'ext01',
-    autoStart: true,
-    requires: [ICommandPalette],
+  id: "ext01",
+  autoStart: true,
+  requires: [ICommandPalette],
 
-    activate: (app: JupyterLab, palette: ICommandPalette) => {
-        console.log('JupyterLab extension jupyterlab_xkcd is activated!');
+  activate: (app: JupyterLab, palette: ICommandPalette) => {
+    console.log("JupyterLab extension jupyterlab_xkcd is activated!");
 
-        // Create a single widget
-        let widget: Widget = new Widget();
-        widget.id = 'xkcd-jupyterlab';
-        widget.title.label = 'xkcd.com';
-        widget.title.closable = true;
+    // Create a single widget
+    let widget: Widget = new Widget();
+    widget.id = "xkcd-jupyterlab";
+    widget.title.label = "xkcd.com";
+    widget.title.closable = true;
+    widget.addClass('jp-xkcdWidget');
 
-        // Add an application command
-        const command: string = 'xkcd:open';
-        app.commands.addCommand(command, {
-            label: 'Random xkcd comic',
-            execute: () => {
-                if (!widget.isAttached) {
-                    // Attach the widget to the main work area if it's not there
-                    app.shell.addToMainArea(widget);
-                }
-                // Activate the widget
-                app.shell.activateById(widget.id);
-            }
-        });
+    // Add an image element to the panel
+    let img = document.createElement("img");
+    img.className = 'jp-xkcdCartoon';
+    widget.node.appendChild(img);
 
-        // Add the command to the palette.
-        palette.addItem({command, category: 'Tutorial'});
-    }
+    // New: add an attribution badge
+    img.insertAdjacentHTML('afterend',
+      `<div class="jp-xkcdAttribution">
+      <a href="https://creativecommons.org/licenses/by-nc/2.5/" class="jp-xkcdAttribution" target="_blank">
+        <img src="https://licensebuttons.net/l/by-nc/2.5/80x15.png" />
+      </a>
+    </div>`
+    );
 
+    // Fetch info about a random comic
+    fetch("https:////egszlpbmle.execute-api.us-east-1.amazonaws.com/prod")
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        img.src = data.img;
+        img.alt = data.title;
+        img.title = data.alt;
+      });
+
+    // Add an application command
+    const command: string = "xkcd:open";
+    app.commands.addCommand(command, {
+      label: "Random xkcd comic",
+      execute: () => {
+        if (!widget.isAttached) {
+          // Attach the widget to the main work area if it's not there
+          app.shell.addToMainArea(widget);
+        }
+        // Activate the widget
+        app.shell.activateById(widget.id);
+      }
+    });
+
+    // Add the command to the palette.
+    palette.addItem({ command, category: "Tutorial" });
+  }
 };
 
 export default extension;
